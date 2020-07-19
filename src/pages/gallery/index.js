b5fbfd380117a27fb12cee7bd1ad85873dad8994
Allan Joshua <calla@amazon.com>
2020-07-20 01:24:00 +0530

    Added lightbox view to gallery with mock images

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,42 +1,93 @@
 import * as React from 'react';
 import { Typography, Input } from 'antd';
 import PhotoGallery from 'react-photo-gallery';
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 const { Search } = Input;
 const { Text } = Typography;
-const getPhotos = () => [ ...new Array(50) ].map((item, index) => {
-  const items = [1, 3, 4];
-  const width = items[Math.floor(Math.random() * items.length)];
-  const height = items[Math.floor(Math.random() * items.length)];
-  return {
-    src: String(index),
-    width,
-    height
+const photos = [
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    width: 1,
+    height: 1
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3
   }
-})
+];
 
-export const Gallery = () => <>
-  <div className='horizontal-center-align'>
-    <div className='flex-column'>
-      <Text style={{ fontSize: '20px' }}>Explore our collection of exclusive dresses and sarees & find your style</Text>
-      <Search
-        placeholder="Ex. Red Lehenga"
-        size='large'
+export class Gallery extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showGallery: false,
+      galleryStartIndex: 0
+    }
+  }
+  render = () => {
+    return <>
+      <div className='horizontal-center-align'>
+        <div className='flex-column'>
+          <Text style={{ fontSize: '20px' }}>Explore our collection of exclusive dresses and sarees & find your style</Text>
+          <Search
+            placeholder="Ex. Red Lehenga"
+            size='large'
+          />
+        </div>
+      </div>
+      <br />
+      <br />
+      <PhotoGallery
+        photos={photos}
+        onClick={(e, {index }) => this.setState({ showGallery: true, galleryStartIndex: index })}
       />
-    </div>
-  </div>
-  <br />
-  <br />
-  <PhotoGallery
-    photos={getPhotos()}
-    renderImage={({ photo, index }) => <div
-      key={index}
-      style={{
-        width: photo.width,
-        height: photo.height,
-        backgroundColor: '#fff',
-        margin: '2px'
-      }}
-    />}
-  />
-</>;
+      <ModalGateway>
+        {this.state.showGallery ? (
+          <Modal onClose={() => this.setState({ showGallery: false, galleryStartIndex: 0 })}>
+            <Carousel
+              currentIndex={this.state.galleryStartIndex}
+              views={photos}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </>;
+  }
+}
